16a190ff495f4912fef428f788a96fa8da264fe8
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-29 11:49:20 +0200

    feat(jest test) : ajout du test texte chargement si on clique sur connexion

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,19 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 
 const Login = () => {
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState({});
 
   const handleClick = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-
+      const data = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+      setUser(data);
     }catch{
       setError(true)
     }
@@ -16,10 +21,11 @@ const Login = () => {
 
   return (
     <div className='container'>
+      <span>{user.name}</span>
       <form>
         <input type="text" placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
         <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)}/>
-        <button disabled={!username || !password} onClick={handleClick}>Login</button>
+        <button disabled={!username || !password} onClick={handleClick}>{loading ? "please wait" : "Login" }</button>
         <span data-testid="error" style={{visibility: error ? "visible" : "hidden"}}>Something went wrong</span>
       </form>
     </div>
--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -37,6 +37,12 @@ test('button should be disabled', () => {
 	expect(buttonEl).toBeDisabled();
 });
 
+test('loading should not be rendered', () => {
+	render(<Login />);
+	const buttonEl = screen.getByRole('button');
+	expect(buttonEl).not.toHaveTextContent(/please wait/i);
+});
+
 test('error message should not be visible', () => {
 	render(<Login />);
 	const errorEl = screen.getByTestId('error');
@@ -73,4 +79,19 @@ test('button should not be disabled when input exist', () => {
 	fireEvent.change(passwordInputEl, { target: { value: testValue } });
 
 	expect(buttonEl).not.toBeDisabled();
+});
+
+test('loading should be rendered when click', () => {
+	render(<Login />);
+	const buttonEl = screen.getByRole('button');
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+	const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+	const testValue = 'test';
+
+	fireEvent.change(usernameInputEl, { target: { value: testValue } });
+	fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+	expect(buttonEl).toHaveTextContent(/please wait/i);
 });
