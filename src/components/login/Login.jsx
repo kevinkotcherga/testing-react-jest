a6b92dc6b4d606e194f9738e9d3ee8233bde5bfe
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-29 11:28:33 +0200

    feat(jest test) : ajout du test bouton login visible quand username et mot de passe remplit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+
+    }catch{
+      setError(true)
+    }
+  };
+
   return (
     <div className='container'>
       <form>
-        <input type="text" placeholder='username'/>
-        <input type="password" placeholder='password'/>
-        <button>Login</button>
+        <input type="text" placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
+        <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)}/>
+        <button disabled={!username || !password} onClick={handleClick}>Login</button>
+        <span data-testid="error" style={{visibility: error ? "visible" : "hidden"}}>Something went wrong</span>
       </form>
     </div>
   )
--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Login from "./Login"
 
 test('usernam input should be rendered', () => {
@@ -15,18 +15,62 @@ test('password input should be rendered', () => {
 
 test('button should be rendered', () => {
 	render(<Login />);
-	const buttonInputEl = screen.getByRole("button");
-	expect(buttonInputEl).toBeInTheDocument();
+	const buttonEl = screen.getByRole("button");
+	expect(buttonEl).toBeInTheDocument();
 });
 
 test('username input should be empty', () => {
 	render(<Login />);
-	const userInputEl = screen.getByPlaceholderText(/username/i);
-	expect(userInputEl.value).toBe("");
+	const usernameInputEl = screen.getByPlaceholderText(/username/i);
+	expect(usernameInputEl.value).toBe("");
 });
 
 test('password input should be empty', () => {
 	render(<Login />);
-	const userInputEl = screen.getByPlaceholderText(/password/i);
-	expect(userInputEl.value).toBe('');
+	const passwordInputEl = screen.getByPlaceholderText(/password/i);
+	expect(passwordInputEl.value).toBe('');
+});
+
+test('button should be disabled', () => {
+	render(<Login />);
+	const buttonEl = screen.getByRole('button');
+	expect(buttonEl).toBeDisabled();
+});
+
+test('error message should not be visible', () => {
+	render(<Login />);
+	const errorEl = screen.getByTestId('error');
+	expect(errorEl).not.toBeVisible()
+});
+
+test('username input should change', () => {
+	render(<Login />);
+	const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, {target:{value: testValue }});
+	expect(usernameInputEl.value).toBe(testValue);
+});
+
+test('password input should change', () => {
+	render(<Login />);
+	const passwordInputEl = screen.getByPlaceholderText(/password/i);
+	const testValue = 'test';
+
+	fireEvent.change(passwordInputEl, { target: { value: testValue } });
+	expect(passwordInputEl.value).toBe(testValue);
+});
+
+test('button should not be disabled when input exist', () => {
+	render(<Login />);
+	const buttonEl = screen.getByRole('button');
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+	const testValue = 'test';
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+	fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+	expect(buttonEl).not.toBeDisabled();
 });
